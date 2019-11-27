348d4635781f7ab2fe1531096f59e97c6b4fa2e4
edgarmad <edmad1396@gmail.com>
2019-11-27 12:31:01 -0600

    modificando test again2

--- src/test/java/com/mayab/calidad/travis/AlumnoDbTest.java
+++ src/test/java/com/mayab/calidad/travis/AlumnoDbTest.java
@@ -21,7 +21,7 @@ import org.junit.Before;
 public class AlumnoDbTest extends DBTestCase {
     
     public String URL="jdbc:mysql://localhost:3306/calidad2"+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    public AlumnoDbTest2(String name){
+    public AlumnoDbTest(String name){
         super(name);
         System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver");
         System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, URL);
