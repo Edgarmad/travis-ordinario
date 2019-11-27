9b717e18f1cac33714a03496beef6cbfadc99267
edgarmad <edmad1396@gmail.com>
2019-11-27 11:56:48 -0600

    prueba de pruebas

--- src/main/java/com/mayab/calidad/travis/Alumno.java
+++ src/main/java/com/mayab/calidad/travis/Alumno.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mayab.calidad.dbunit;
+package com.mayab.calidad.travis;
 
 /**
  *
