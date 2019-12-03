7a418219e92657f4a77f9a18e09a30a49d5ab352
Edgarmad <54693845+Edgarmad@users.noreply.github.com>
2019-12-03 15:07:29 -0600

    chance2

--- src/main/java/com/mayab/calidad/travis/dbunit/AlumnoDAO.java
+++ src/main/java/com/mayab/calidad/travis/dbunit/AlumnoDAO.java
@@ -1,8 +1,8 @@
 package com.mayab.calidad.travis.dbunit;
 public interface AlumnoDAO {
-	public Void addAlumno(Alumno alumno);
-	public Void removeAlumno(String Alumno, String lastName);
-	public Void updateAlumnoPromedio(Alumno alumno, Float promedio);
-	public Void getAlumno(String id);
+	public void addAlumno(Alumno alumno);
+	public void removeAlumno(String Alumno, String lastName);
+	public void updateAlumnoPromedio(Alumno alumno, Float promedio);
+	public void getAlumno(String id);
 	
 }
